19b4f6aba026cf0794e6aeed463356143398af9d
Anna-0301 <annabogd123@gmail.com>
2024-11-14 15:48:06 +0500

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final int SIZE_OF_EMPTY = 10;
+    public static final String NEW_VALUE = "";
+
+    public static String surname() {
+        String name = "";
+        return name;
+    }
+
+    public static void echo() {
+    }
+
+    public static void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public static void method() {
+        int a, b, c, d, e, f, g, h;
+    }
+
+}
