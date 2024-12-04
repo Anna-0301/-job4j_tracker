c6245f9a6ae77623c12e9e199948abd6bb30e56c
Anna-0301 <annabogd123@gmail.com>
2024-12-04 10:45:51 +0500

    5. Tracker - хранилище [#396 #524359]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,25 +14,15 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[items.length];
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                result = items;
-                break;
-            }
-        }
-        return Arrays.copyOf(result, size);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
         Item[] result = new Item[items.length];
         int count = 0;
         for (int i = 0; i < size; i++) {
-            if (items[i] == null) {
-                break;
-            }
             if (items[i].getName().equals(key)) {
-                result = items;
+                result[count] = items[count];
                 count++;
             }
         }
