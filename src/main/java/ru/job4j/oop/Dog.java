82630a3a1ef63c06e56654f64e90bc8d81ed775e
Anna-0301 <annabogd123@gmail.com>
2025-01-15 13:35:52 +0500

    ---

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -1,9 +1,22 @@
 package ru.job4j.oop;
 
 public class Dog {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Dog polkan = new Dog();
         Dog sharik = new Dog();
         Dog zhychka = new Dog();
+    }*/
+        public static void main(String[] args) {
+            String name = "Dog";
+            change(name);
+            System.out.print(name);
+            name = change(name);
+            System.out.print(name);
+        }
+
+        public static String change(String name) {
+            name = name + "Cat";
+            String surname = "Kotov";
+            return surname;
+        }
     }
-}
