bbc366e9c317ced583c70b1dab4c4c51e259dc99
Anna-0301 <annabogd123@gmail.com>
2025-01-28 16:57:29 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("olegivanov@yandex.ru", "Oleg Ivanov");
+        map.put("irinam2002@yandex.ru", "Irina Maslova");
+        map.put("svan1919@yandex.ru", "Lily Swan");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + ", " + value);
+        }
+    }
+}
