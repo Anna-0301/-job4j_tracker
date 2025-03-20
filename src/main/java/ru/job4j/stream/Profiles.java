980f56dc676b5052229089358a453ebbef9704bd
Anna-0301 <annabogd123@gmail.com>
2025-03-20 10:05:46 +0500

    6. Список адресов [#110062]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,10 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(p -> new Address(p.getAddress().getCity(), p.getAddress().getStreet(), p.getAddress().getHome(), p.getAddress().getApartment()))
-                .collect(Collectors.toList());
+                .map(Profile::getAddress).toList();
     }
 }
